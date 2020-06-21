1e9983283ec7370e7c10fae48cd789b53fd69106
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-06-21 01:26:43 +0100

    initial commit

---
+++ src/main/java/com/bankwithmint/mintconsumer/MintconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bankwithmint.mintconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintconsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankwithmint/mintconsumer/MintconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankwithmint.mintconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
